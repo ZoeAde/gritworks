48a11cd3c8f1b9221e43b67bd8d0b4b17a39cdc9
ZoeAde <zoe.a.adelman@gmail.com>
2016-04-29 11:46:01 -0600

    adding google form

--- js/main.js
+++ js/main.js
@@ -2,4 +2,11 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+    // MailApp.sendEmail("zoe.a.adelman@gmail.com", "New Submission", JSON.stringify(e.parameters));
+
+    $('#postForm').on('click', function() {
+      console.log('form click');
+    });
+
+
 });
